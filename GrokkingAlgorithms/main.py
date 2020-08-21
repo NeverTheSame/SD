8fa7d89e3f5099b3df5116b4d727bfeea088baae
Kirill <kirill.kuklin@yahoo.com>
2020-08-20 18:04:43 -0700

    Selection sort

--- GrokkingAlgorithms/main.py
+++ GrokkingAlgorithms/main.py
@@ -1,23 +1,43 @@
-def binary_search(list, item):
+my_list = [1, 2, 11, 3, 5, 7, 8, 9, 0]
+
+
+def binary_search(list_of_ints, item):
+    """ Method for doing binary search."""
     low_index = 0
-    high_index = len(list) - 1
+    high_index = len(list_of_ints) - 1
 
     while low_index <= high_index:
-        print("\tNew iteration, l, h", low_index, high_index)
         mid = int((low_index + high_index) / 2)  # rounded down by Python automatically if low_index + high_index is uneven
-        print("Mid:", int(mid))
-        guess = list[int(mid)]
-        print("Guessing the number:", guess)
+        guess = list_of_ints[int(mid)]
         if guess == item:
             return mid
         if guess > item:
             high_index = int(mid - 1)
-            print("High is set to:", high_index)
         else:
             low_index = mid + 1
     return None
 
 
-my_list = [1, 2, 3, 5, 7, 8, 9]
-print(binary_search(my_list, 1))
-# print(binary_search(my_list, -1))
+def findSmallest(arr):
+    """Find the smallest element in an array"""
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    """Sorts an array"""
+    new_arr = []
+    for i in range(len(arr)):
+        """Finds the smallest element in the array and adds it to the new array"""
+        smallest = findSmallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(selectionSort(my_list))
+
