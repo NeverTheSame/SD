7926a4965cb66ad1b12c00734883ea60c3b69a92
Kirill <kirill.kuklin@yahoo.com>
2021-02-23 16:14:01 -0800

    intermediate commit

--- PS Scripts/Test.ps1
+++ PS Scripts/Test.ps1
@@ -1,9 +1,3 @@
-# Enter-PSSession 172.16.224.195
-# Invoke-Command get-service -ComputerName 172.16.224.195
-
-#New-PSSession -ComputerName 172.16.224.195 -Credential .\administrator -Password 642362kkK!
-
-$pw = convertto-securestring -AsPlainText -Force -String 642362kkK!
+$pw = convertto-securestring -AsPlainText -Force -String "123"
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist ".\Administrator",$pw
-$session = new-pssession -computername 172.16.224.195 -credential $cred
-# Invoke-Command -Session $s -ScriptBlock {Get-Culture}
+$session = new-pssession -computername "123" -credential $cred
