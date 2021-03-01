bf2e981b6bfdc8bc521fb5cb31234b7eb83abb0d
Kirill <kirill.kuklin@yahoo.com>
2021-03-01 14:08:15 -0800

    reads both TXT and LOG files

---
+++ Komatik/Komatik/Komatik/ConsolePrinter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+ namespace Komatik
+{
+    static class ConsolePrinter
+    {
+        public static void SetTitle()
+        {
+            Console.Title = "";
+        }
+
+        public static void PrintSeparator()
+        {
+            Console.WriteLine(new string('-', 60));
+        }
+        
+        public static void PrintInvoiceListingMessage()
+        {
+            Console.WriteLine("Invoice Listing");
+        }
+        
+        public static void PrintEmptyLine()
+        {
+            Console.WriteLine("");
+        }
+
+        public static void PrintNoArgsMessage()
+        {
+            Console.WriteLine("\nNo command line arguments provided.\n");
+        }
+
+        public static void PrintFileNotFoundMessage()
+        {
+            Console.WriteLine("\nFile not found.\n");
+        }
+
+        public static void PrintExceptionMessage(Exception ex)
+        {
+            Console.WriteLine($"\n{ex.Message}\n");
+        }
+
+        public static void PrintColumnNames()
+        {
+            Console.WriteLine("{0, 3} {1, -11} {2, -18} {3, 12} {4, -8} {5, -2}",
+                "Qty", "SKU", "Description", "Price", "PST", "Ext");
+        }
+        
+
+        public static void PressKeyToExit()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+
+        public static void PrintCalculatedInvoiceData(string fieldName, double value) {
+            Console.WriteLine($"{"", 3} " +
+                              $"{"", -11} " +
+                              $"{fieldName, -18} " +
+                              $"{"", 12} " +
+                              $"{"", 2} " +
+                              $"{value, 9:N}");
+        }
+    }
+}
--- Komatik/Komatik/Komatik/Program.cs
+++ Komatik/Komatik/Komatik/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using static Komatik.ConsolePrinter;
+using System.IO;
+
 
 namespace Komatik {
     class Program {
+        
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            // checking command line arguments presence
+            if (args.Length == 0) {
+                PrintNoArgsMessage();
+            }
+            else {
+                // parsing first arg, supposedly a filename
+                var fileNameInDebugDir = args[0];
+                StreamReader streamReader = null;
+
+                // checking file presence
+                if (!File.Exists(fileNameInDebugDir)) {
+                    PrintFileNotFoundMessage();
+                }
+                // file was found, processing with data operations
+                else {
+                    // read entire file line-by-line
+                    try {
+                        // Attempt to open a file. It will throw an exception if fails,
+                        // hence try-catch block.
+                        streamReader = new StreamReader(fileNameInDebugDir);
+                        var receiveRate = "Receive rate"; // download rate from production
+                        var savingRate = "Saving rate"; // upload rate to the target 
+                        string lineData;
+                        while ((lineData = streamReader.ReadLine()) != null) {
+
+                            var linesInLog = lineData.Split('\n');
+                            for (var line = 0; line < linesInLog.Length; line++) {
+                                var logLine = linesInLog[line];
+                                Console.WriteLine(logLine);
+                            }
+                        }
+                    }
+                    catch (Exception ex) {
+                        PrintExceptionMessage(ex);
+                    }
+                    // closing stream reader so that file lock is released.
+                    finally {
+                        streamReader.Close();
+                    }
+                }
+
+
+                // Keep the console window open, if required.
+                // PressKeyToExit();
+            }
         }
     }
 }
